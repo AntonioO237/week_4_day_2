f6304f505e94c105ff81d32534884695e940f255
Olvera <aolvera22@cps.k12.il.us>
2024-09-20 09:31:51 -0500

    first commit

--- week_4_day_1/week_4_assessment/mini_challenge_Wk4.py
+++ week_4_day_1/week_4_assessment/mini_challenge_Wk4.py
@@ -2,31 +2,42 @@
 
 # Extracting Sub-Strings Practice #1
 # Extract the first word of the following sentence using slicing, and display it on the screen:
-# "Controlling complexity is the essence of programming"
+sentence = "Controlling complexity is the essence of programming"
+substring = sentence.find("Contolling")
+print(substring)
+substring = (sentence[0:11])
+print(substring)
 # Hint: "Controlling" is 11 characters long.
 
 # Extracting Sub-Strings Practice #2
 # Take every third character starting from the ninth to the end of the sentence, and print the result.
-# "Never trust a computer you can't throw out a window"
+text = "Never trust a computer you can't throw out a window"
+print(text[0:-1:3])
 
 # Extracting Sub-Strings Practice #3
 # Reverses the position of all the characters in the following sentence and displays the result on the screen.
-# "It's great to work with computers. They don't argue, they remember everything and they don't drink your beer"
-
+text = "It's great to work with computers. They don't argue, they remember everything and they don't drink your beer"
+print(text[::-1])
 ##################################### String Methods#################################
+
 # String Methods Practice #1
-#slieds 12 -16
+# Slides 12 -16
 # Print the following text in uppercase, using the specific string method:
-# "Especially in electronic communications, writing in all caps is equivalent to yelling."
+text = "Especially in electronic communications, writing in all caps is equivalent to yelling."
+print(text.upper())
 # sentence = "Especially in electronic communications, writing in all caps is equivalent to yelling."
 
 # String Methods Practice #2
 # Join the following list into a string, separating each item with a space. Use the appropriate list/string method, and display the result.
-# word_list = ["Simple","is","better","than","complex."]
+word_list = ("Simple","is","better","than","complex.")
+joined_list = " ".join(word_list)
+print(joined_list)
 
 # String Methods Practice #3
 # Replace in the following sentence:
-# "If the implementation is hard to explain, it might be a bad idea."
+sentence4 = "If the implementation is hard to explain, it might be a bad idea."
+new_sentence = sentence4.replace("hard", "easy").replace("bad", "good")
+print(new_sentence)
 # the following pairs of words:
 # "hard" --> "easy"
 # "bad" --> "good"
@@ -37,6 +48,8 @@
 # String Properties Practice #1
 # Concatenate the text "Repetition" 15 times and display the result on the screen.
 # Luckily, you know that strings are multipliable and you can do this activity in a simple and elegant way.
+repetition = "Repetition" * 15
+print(repetition)
 
 # String Properties Practice #2
 # Check if the word "beach" is not found in the following haiku. You should print the boolean.
@@ -47,5 +60,4 @@
 
 # String Properties Practice #3
 # Check the Python Documentation to find the description of the len() function. Then, display on the screen the length (in number of characters) of the word electroencephalographist.
-
-################################list###################################################
+word = "electroencephalographist"
